1b458a8fc0bb4d5f6ff346ebfe97283bd7c7dc49
Juris Guevara <60456852+JurisGuevara@users.noreply.github.com>
2021-04-21 04:35:20 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,37 @@
+function field_focus(field, email){
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+}
+
+function field_blur(field, email){
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+}
+
+function login(){
+    const email = document.getElementById("emailInput").value
+    const password = document.getElementById("passwordInput").value
+    const message = document.getElementById("validation-message")
+    if(email === 'cafe24' && password === '1234'){
+        message.style.color = "#00ff72"
+        message.innerHTML = 'Login successful'
+    }
+    else{
+        message.style.color = "red"
+        message.innerHTML = 'Invalid username or password'
+    }
+}
+
+//Fade in dashboard box
+// $(document).ready(function(){
+//     $('.box').hide().fadeIn(1000);
+//     });
+
+// //Stop click event
+// $('a').click(function(event){
+//     event.preventDefault(); 
+// 	});
