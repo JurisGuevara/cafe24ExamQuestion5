1e9e898ef452cfb65bbcc8cb1480fda649d63999
Juris Guevara <60456852+JurisGuevara@users.noreply.github.com>
2021-04-21 04:38:31 +0800

    Update login.js

--- login.js
+++ login.js
@@ -25,13 +25,3 @@ function login(){
         message.innerHTML = 'Invalid username or password'
     }
 }
-
-//Fade in dashboard box
-// $(document).ready(function(){
-//     $('.box').hide().fadeIn(1000);
-//     });
-
-// //Stop click event
-// $('a').click(function(event){
-//     event.preventDefault(); 
-// 	});
